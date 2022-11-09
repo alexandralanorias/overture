52c8e97248af44f1c91347989d4cfe1fe3cf53ab
alexandra <97576137+alexandralanorias@users.noreply.github.com>
2022-11-09 10:44:37 +0800

    Initializing express backend

--- app.js
+++ app.js
@@ -9,6 +9,26 @@ const app = express();
 // Indicate the port number server will run on
 const PORT = process.env.PORT || 3000;
 
+// Set template engine
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
+// Needed to parse html data for POST requests
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+// GET route
+app.get("/", (req, res) => {
+    res.send("Mendelssohn Violin Concerto");
+});
+
+// POST route
+app.post("/", (req, res) => {
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
